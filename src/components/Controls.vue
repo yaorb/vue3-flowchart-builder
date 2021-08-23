cc027a904188144e965439493c791b15dac94888
Simon Porritt <simon@jsplumbtoolkit.com>
2021-08-24 09:56:23 +1000

    remove unused template component.  document the internals a little.

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="controls" ref="container">
-        <h1>hello</h1>
         <i class="fa fa-arrows selected-mode" mode="pan" title="Pan Mode" v-on:click="panMode()"></i>
         <i class="fa fa-pencil" mode="select" title="Select Mode" v-on:click="selectMode()"></i>
         <i class="fa fa-home" reset title="Zoom To Fit" v-on:click="zoomToFit()"></i>
@@ -27,6 +26,9 @@
         loadSurface(surfaceId, cb)
     }
 
+    /**
+     Provides the various buttons on the top left of the demo - pan/lasso mode, undo/redo, zoom to fit and clear dataset.
+     */
     export default defineComponent({
         props:["surfaceId"],
         methods:{
@@ -60,11 +62,18 @@
             container = this.$refs.container;
             _getSurface((surface) => {
 
+                //
+                // bind to undo/redo events from the toolkit and set an attribute whose value depends on the number
+                // of entries in the undo stack. via CSS we style/disable/enable the undo/redo buttons accordingly.
+                //
                 surface.toolkitInstance.bind(EVENT_UNDOREDO_UPDATE, (state) => {
                     container.setAttribute("can-undo", state.undoCount > 0 ? "true" : "false")
                     container.setAttribute("can-redo", state.redoCount > 0 ? "true" : "false")
                 })
 
+                //
+                // when the user clicks whitespace, clear the current selection.
+                //
                 surface.bind(EVENT_CANVAS_CLICK, () => {
                     surface.toolkitInstance.clearSelection()
                 });
