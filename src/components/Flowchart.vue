f395a9216011c0d9a25d1d27959e39489107c7d2
yaorb <yaorb@msn.com>
2022-11-25 16:48:08 +0800

    ...

--- src/components/Flowchart.vue
+++ src/components/Flowchart.vue
@@ -1,200 +1,208 @@
 <template>
-    <div style="width: 100%;height: 100%;position: relative;">
-        <jsplumb-toolkit ref="toolkitComponent"
-                         url="data/copyright.json"
-                         id="toolkit"
-                         surface-id="surface"
-                         v-bind:render-params="this.renderParams()"
-                         v-bind:view="this.viewParams()"
-                         v-bind:toolkit-params="this.toolkitParams()">
-        </jsplumb-toolkit>
-        <jsplumb-miniview surface-id="surface"></jsplumb-miniview>
-    </div>
+  <div style="width: 100%; height: 100%; position: relative">
+    <jsplumb-toolkit
+      ref="toolkitComponent"
+      url="data/copyright.json"
+      id="toolkit"
+      surface-id="surface"
+      v-bind:render-params="this.renderParams()"
+      v-bind:view="this.viewParams()"
+      v-bind:toolkit-params="this.toolkitParams()"
+    >
+    </jsplumb-toolkit>
+    <jsplumb-miniview surface-id="surface"></jsplumb-miniview>
+  </div>
 </template>
 
 <script>
+import { loadSurface } from "@jsplumbtoolkit/browser-ui-vue3";
+import {
+  ArrowOverlay,
+  BlankEndpoint,
+  LabelOverlay,
+  AnchorLocations,
+  EVENT_CLICK,
+  EVENT_TAP,
+} from "@jsplumbtoolkit/browser-ui";
+import { SpringLayout } from "@jsplumbtoolkit/layout-spring";
+import { LassoPlugin } from "@jsplumbtoolkit/browser-ui-plugin-lasso";
+import { DrawingToolsPlugin } from "@jsplumbtoolkit/browser-ui-plugin-drawing-tools";
+import { EdgePathEditor } from "@jsplumbtoolkit/connector-editors";
+import { OrthogonalConnector } from "@jsplumbtoolkit/connector-orthogonal";
+import * as OrthogonalConnectorEditor from "@jsplumbtoolkit/connector-editors-orthogonal";
 
-    import { loadSurface  } from '@jsplumbtoolkit/browser-ui-vue3';
-    import { ArrowOverlay, BlankEndpoint, LabelOverlay, AnchorLocations, EVENT_CLICK, EVENT_TAP } from "@jsplumbtoolkit/browser-ui";
-    import { SpringLayout } from "@jsplumbtoolkit/layout-spring";
-    import { LassoPlugin } from "@jsplumbtoolkit/browser-ui-plugin-lasso";
-    import { DrawingToolsPlugin } from "@jsplumbtoolkit/browser-ui-plugin-drawing-tools";
-    import { EdgePathEditor } from '@jsplumbtoolkit/connector-editors';
-    import { OrthogonalConnector } from '@jsplumbtoolkit/connector-orthogonal';
-    import * as OrthogonalConnectorEditor from '@jsplumbtoolkit/connector-editors-orthogonal'
+import StartNode from "./StartNode.vue";
+import ActionNode from "./ActionNode.vue";
+import QuestionNode from "./QuestionNode.vue";
+import OutputNode from "./OutputNode.vue";
 
-    import StartNode from './StartNode.vue'
-    import ActionNode from './ActionNode.vue'
-    import QuestionNode from './QuestionNode.vue'
-    import OutputNode from './OutputNode.vue'
+import { defineComponent } from "vue";
 
-    import { defineComponent } from "vue";
+//
+// This call is required in order for the edge editor to register itself with the Toolkit.
+//
+OrthogonalConnectorEditor.initialize();
 
-    //
-    // This call is required in order for the edge editor to register itself with the Toolkit.
-    //
-    OrthogonalConnectorEditor.initialize();
+const TARGET = "target";
+const SOURCE = "source";
+const START = "start";
+const SELECTABLE = "selectable";
+const RESPONSE = "response";
+const OUTPUT = "output";
+const QUESTION = "question";
+const ACTION = "action";
+const DEFAULT = "default";
 
-    const TARGET = 'target';
-    const SOURCE = 'source';
-    const START = 'start';
-    const SELECTABLE = 'selectable';
-    const RESPONSE = 'response';
-    const OUTPUT = 'output';
-    const QUESTION = 'question';
-    const ACTION = 'action';
-    const DEFAULT = "default";
+let toolkitComponent;
+let toolkit;
+let surface;
+let edgeEditor;
 
-    let toolkitComponent;
-    let toolkit;
-    let surface;
-    let edgeEditor;
-
-    export default defineComponent({
-
-        name: 'jsp-toolkit',
-        props:["surfaceId"],
-        inject:['service'],
-        methods:{
-            viewParams:function() {
-                return {
-                    nodes: {
-                        [START]: {
-                            component:StartNode
-                        },
-                        [SELECTABLE]: {
-                            events: {
-                                [EVENT_TAP]: (params) => {
-                                    toolkit.toggleSelection(params.obj);
-                                }
-                            }
-                        },
-                        [QUESTION]: {
-                            parent: SELECTABLE,
-                            component:QuestionNode
-                        },
-                        [ACTION]: {
-                            parent: SELECTABLE,
-                            component:ActionNode
-                        },
-                        [OUTPUT]:{
-                            parent:SELECTABLE,
-                            component:OutputNode
-                        }
-                    },
-                    edges: {
-                        [DEFAULT]: {
-                            anchor:AnchorLocations.AutoDefault,
-                            endpoint:BlankEndpoint.type,
-                            connector: {type:OrthogonalConnector.type, options:{ cornerRadius: 5 } },
-                            paintStyle: { strokeWidth: 2, stroke: "rgb(132, 172, 179)", outlineWidth: 3, outlineStroke: "transparent" },	//	paint style for this edge type.
-                            hoverPaintStyle: { strokeWidth: 2, stroke: "rgb(67,67,67)" }, // hover paint style for this edge type.
-                            events: {
-                                [EVENT_CLICK]:(p) => {
-                                    edgeEditor.startEditing(p.edge, {
-                                        deleteButton:true,
-                                        onMaybeDelete:(edge, connection, doDelete) => {
-                                            this.service.confirmDeleteEdge(edge, doDelete);
-                                        }
-                                    });
-                                }
-                            },
-                            overlays: [
-                                { type:ArrowOverlay.type, options:{ location: 1, width: 10, length: 10 } }
-                            ]
-                        },
-                        [RESPONSE]:{
-                            parent:DEFAULT,
-                                overlays:[
-                                {
-                                    type: LabelOverlay.type,
-                                    options: {
-                                        label: "${label}",
-                                        events: {
-                                            [EVENT_CLICK]: (p) => {
-                                                this.service.editEdge(p.edge.data, (data) => {
-                                                    toolkit.updateEdge(p.edge, data);
-                                                }, () => null)
-                                            }
-                                        }
-                                    }
-                                }
-                            ]
-                        }
-                    },
-
-                    ports: {
-                        [START]: {
-                            edgeType: DEFAULT
-                        },
-                        [SOURCE]: {
-                            maxConnections: -1,
-                            edgeType: RESPONSE
-                        },
-                        [TARGET]: {
-                            maxConnections: -1,
-                            isTarget: true
-                        }
-                    }
-                }
+export default defineComponent({
+  name: "jsp-toolkit",
+  props: ["surfaceId"],
+  inject: ["service"],
+  methods: {
+    viewParams: function () {
+      return {
+        nodes: {
+          [START]: {
+            component: StartNode,
+          },
+          [SELECTABLE]: {
+            events: {
+              [EVENT_TAP]: (params) => {
+                toolkit.toggleSelection(params.obj);
+              },
             },
-            toolkitParams:function() {
-                return {
-                    nodeFactory: this.service.nodeFactory,
-                    beforeStartConnect: (node) => {
-                        // limit edges from start node to 1. if any other type of node, return
-                        return (node.data.type === START && node.getEdges().length > 0) ? false : {label: "..."};
-                    },
-                    edgeFactory: (type, data, continueCallback, abortCallback) => this.service.editEdge(data, continueCallback, abortCallback)
-                }
-            },
-            renderParams:function() {
-                return {
-                    layout:{
-                        type:SpringLayout.type
-                    },
-                    plugins: [
-                        DrawingToolsPlugin.type,
-                        {
-                            type:LassoPlugin.type,
-                            options:{
-                                invert:true
-                            }
-                        }
-                    ],
-                    events:{
-                        canvasClick:() => {
-                            toolkit.clearSelection();
-                            edgeEditor.stopEditing();
-                        }
-                    },
-                    consumeRightClick: false,  // this is a dev setting - it just allows you to right-click and inspect. In production you might want to take this out (the default value is `true`)
-                    dragOptions: {
-                        filter: ".jtk-draw-handle, .node-action, .node-action i"
-                    },
-                    zoomToFit:true,
-                    grid:{
-                        size:{
-                            w:20,
-                            h:20
-                        }
-                    },
-                    magnetize:{
-                        afterDrag:true
-                    }
-                }
-            }
+          },
+          [QUESTION]: {
+            parent: SELECTABLE,
+            component: QuestionNode,
+          },
+          [ACTION]: {
+            parent: SELECTABLE,
+            component: ActionNode,
+          },
+          [OUTPUT]: {
+            parent: SELECTABLE,
+            component: OutputNode,
+          },
         },
-        mounted() {
+        edges: {
+          [DEFAULT]: {
+            anchor: AnchorLocations.AutoDefault,
+            endpoint: BlankEndpoint.type,
+            connector: { type: OrthogonalConnector.type, options: { cornerRadius: 5 } },
+            paintStyle: { strokeWidth: 2, stroke: "rgb(132, 172, 179)", outlineWidth: 3, outlineStroke: "transparent" }, //	paint style for this edge type.
+            hoverPaintStyle: { strokeWidth: 2, stroke: "rgb(67,67,67)" }, // hover paint style for this edge type.
+            events: {
+              [EVENT_CLICK]: (p) => {
+                edgeEditor.startEditing(p.edge, {
+                  deleteButton: true,
+                  onMaybeDelete: (edge, connection, doDelete) => {
+                    this.service.confirmDeleteEdge(edge, doDelete);
+                  },
+                });
+              },
+            },
+            overlays: [{ type: ArrowOverlay.type, options: { location: 1, width: 10, length: 10 } }],
+          },
+          [RESPONSE]: {
+            parent: DEFAULT,
+            overlays: [
+              {
+                type: LabelOverlay.type,
+                options: {
+                  label: "${label}",
+                  events: {
+                    [EVENT_CLICK]: (p) => {
+                      this.service.editEdge(
+                        p.edge.data,
+                        (data) => {
+                          toolkit.updateEdge(p.edge, data);
+                        },
+                        () => null,
+                      );
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        },
 
-            toolkitComponent = this.$refs.toolkitComponent;
-            toolkit = toolkitComponent.toolkit;
+        ports: {
+          [START]: {
+            edgeType: DEFAULT,
+          },
+          [SOURCE]: {
+            maxConnections: -1,
+            edgeType: RESPONSE,
+          },
+          [TARGET]: {
+            maxConnections: -1,
+            isTarget: true,
+          },
+        },
+      };
+    },
+    toolkitParams: function () {
+      return {
+        nodeFactory: this.service.nodeFactory,
+        beforeStartConnect: (node) => {
+          // limit edges from start node to 1. if any other type of node, return
+          return node.data.type === START && node.getEdges().length > 0 ? false : { label: "..." };
+        },
+        edgeFactory: (type, data, continueCallback, abortCallback) =>
+          this.service.editEdge(data, continueCallback, abortCallback),
+      };
+    },
+    renderParams: function () {
+      return {
+        layout: {
+          type: SpringLayout.type,
+        },
+        plugins: [
+          DrawingToolsPlugin.type,
+          {
+            type: LassoPlugin.type,
+            options: {
+              invert: true,
+            },
+          },
+        ],
+        events: {
+          canvasClick: () => {
+            toolkit.clearSelection();
+            edgeEditor.stopEditing();
+          },
+        },
+        consumeRightClick: false, // this is a dev setting - it just allows you to right-click and inspect. In production you might want to take this out (the default value is `true`)
+        dragOptions: {
+          filter: ".jtk-draw-handle, .node-action, .node-action i",
+        },
+        zoomToFit: true,
+        grid: {
+          size: {
+            w: 20,
+            h: 20,
+          },
+        },
+        magnetize: {
+          afterDrag: true,
+        },
+      };
+    },
+  },
+  mounted() {
+    toolkitComponent = this.$refs.toolkitComponent;
+    toolkit = toolkitComponent.toolkit;
 
-            loadSurface(this.surfaceId, (s) => {
-                surface = s;
-                edgeEditor = new EdgePathEditor(s);
-            })
-        }
-    })
-
+    loadSurface(this.surfaceId, (s) => {
+      surface = s;
+      edgeEditor = new EdgePathEditor(s);
+    });
+  },
+});
 </script>
--- src/service.ts
+++ src/service.ts
@@ -1,38 +1,41 @@
-import * as Dialogs from "@jsplumbtoolkit/dialogs"
-import { Node, ObjectData, JsPlumbToolkit, Edge, uuid } from "@jsplumbtoolkit/core"
+import * as Dialogs from "@jsplumbtoolkit/dialogs";
+import { Node, ObjectData, JsPlumbToolkit, Edge, uuid } from "@jsplumbtoolkit/core";
 
 const dialogs = Dialogs.newInstance({
-    dialogs: {
-        "dlgText": {
-            template:'<input type="text" size="50" jtk-focus jtk-att="text" value="${text}" jtk-commit="true"/>',
-            title:'Enter Text',
-            cancelable:true
+  dialogs: {
+    dlgText: {
+      template: '<input type="text" size="50" jtk-focus jtk-att="text" value="${text}" jtk-commit="true"/>',
+      title: "Enter Text",
+      cancelable: true,
+    },
+    dlgConfirm: {
+      template: "${msg}",
+      title: "Please Confirm",
+      cancelable: true,
+    },
+    dlgMessage: {
+      template: "${msg}",
+      title: "Message",
+      cancelable: false,
+    },
+  },
+});
 
-        },
-        "dlgConfirm": {
-            template: '${msg}',
-            title: 'Please Confirm',
-            cancelable: true
-        },
-        "dlgMessage": {
-            template:'${msg}',
-            title:"Message",
-            cancelable:false
-        }
-    }
-})
-
-function showEdgeEditDialog(data:ObjectData, continueFunction:Dialogs.CommitFunction, abortFunction:Dialogs.CancelFunction) {
-    dialogs.show({
-        id: "dlgText",
-        data: {
-            text: data.label || ""
-        },
-        onOK: function (data) {
-            continueFunction({label:data.text || ""})
-        },
-        onCancel:abortFunction
-    });
+function showEdgeEditDialog(
+  data: ObjectData,
+  continueFunction: Dialogs.CommitFunction,
+  abortFunction: Dialogs.CancelFunction,
+) {
+  dialogs.show({
+    id: "dlgText",
+    data: {
+      text: data.label || "",
+    },
+    onOK: function (data) {
+      continueFunction({ label: data.text || "" });
+    },
+    onCancel: abortFunction,
+  });
 }
 
 /**
@@ -40,57 +43,65 @@ function showEdgeEditDialog(data:ObjectData, continueFunction:Dialogs.CommitFunc
  * the main component. `inject/provide` is a new capability that Vue 3 offers which Vue 2 does not have.
  */
 export default {
+  nodeFactory: (
+    type: string,
+    data: ObjectData,
+    callback: Dialogs.CommitFunction,
+    abort?: Dialogs.CancelFunction,
+  ): void => {
+    dialogs.show({
+      id: "dlgText",
+      title: "Enter " + type + " name:",
+      onOK: (d: Record<string, any>) => {
+        data.text = d.text;
+        // if the user entered a name...
+        if (data.text) {
+          // and it was at least 2 chars
+          if (data.text.length >= 2) {
+            // set an id and continue.
+            data.id = uuid();
+            callback(data);
+          } else {
+            // else advise the user.
+            alert(type + " names must be at least 2 characters!");
+          }
+        }
+        // else...do not proceed.
+      },
+    });
+  },
 
-    nodeFactory: (type: string, data: ObjectData, callback: Dialogs.CommitFunction, abort?: Dialogs.CancelFunction):void => {
-        dialogs.show({
-            id: 'dlgText',
-            title: 'Enter ' + type + ' name:',
-            onOK: (d: Record<string, any>) => {
-                data.text = d.text;
-                // if the user entered a name...
-                if (data.text) {
-                    // and it was at least 2 chars
-                    if (data.text.length >= 2) {
-                        // set an id and continue.
-                        data.id = uuid()
-                        callback(data);
-                    } else {
-                        // else advise the user.
-                        alert(type + ' names must be at least 2 characters!');
-                    }
-                }
-                // else...do not proceed.
-            }
-        });
-    },
-
-    editNode:(node:Node, commitFunction:Dialogs.CommitFunction):void => {
-        dialogs.show({
-            id: "dlgText",
-            data: node.data,
-            title: `Edit ${node.data.type} name`,
-            onOK: commitFunction
-        });
-    },
-    editEdge:(data:ObjectData, continueCallback:Dialogs.CommitFunction, abortCallback:Dialogs.CancelFunction):void => {
-        showEdgeEditDialog(data, continueCallback, abortCallback)
-    },
-    maybeDelete:(n:Node, toolkit:JsPlumbToolkit):void => {
-        dialogs.show({
-            id:"dlgConfirm",
-            data:{
-                msg:`Delete ${n.data.text}?`
-            },
-            onOK:() => toolkit.removeNode(n)
-        })
-    },
-    confirmDeleteEdge:(e:Edge, confirm:Dialogs.CommitFunction):void => {
-        dialogs.show({
-            id: "dlgConfirm",
-            data: {
-                msg: "Delete Edge?"
-            },
-            onOK: confirm
-        });
-    }
-}
+  editNode: (node: Node, commitFunction: Dialogs.CommitFunction): void => {
+    dialogs.show({
+      id: "dlgText",
+      data: node.data,
+      title: `Edit ${node.data.type} name`,
+      onOK: commitFunction,
+    });
+  },
+  editEdge: (
+    data: ObjectData,
+    continueCallback: Dialogs.CommitFunction,
+    abortCallback: Dialogs.CancelFunction,
+  ): void => {
+    showEdgeEditDialog(data, continueCallback, abortCallback);
+  },
+  maybeDelete: (n: Node, toolkit: JsPlumbToolkit): void => {
+    dialogs.show({
+      id: "dlgConfirm",
+      data: {
+        msg: `Delete ${n.data.text}?`,
+      },
+      onOK: () => toolkit.removeNode(n),
+    });
+  },
+  confirmDeleteEdge: (e: Edge, confirm: Dialogs.CommitFunction): void => {
+    dialogs.show({
+      id: "dlgConfirm",
+      data: {
+        msg: "Delete Edge?",
+      },
+      onOK: confirm,
+    });
+  },
+};
